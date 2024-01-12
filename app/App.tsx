39a40fc88a8452aaa11026f0e0a4f0b115c76954
Lee Jihye <jihae0584@gmail.com>
2024-01-13 04:21:34 +0900

    메인화면 appbar UI 그림

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,57 @@
-import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { name as appName } from '../app.json';
-import App from './App';
+import {View} from '@gluestack-ui/themed';
+import {Fragment} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Appbar, Button, IconButton, Text} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function Main() {
+const MainAppBar = () => (
+  <View>
+    <Appbar.Header mode="center-aligned" style={{height: 52}}>
+      <Appbar.Action icon="menu" onPress={() => {}} />
+      <Appbar.Content
+        title="내생카"
+        titleStyle={{
+          color: '#FD3636',
+          fontSize: 15,
+          textAlign: 'center',
+        }}
+      />
+    </Appbar.Header>
+    <Appbar.Header
+      style={{
+        justifyContent: 'space-between',
+        alignContent: 'center',
+      }}>
+      <Button
+        compact
+        style={{borderRadius: 0}}
+        contentStyle={{
+          flexDirection: 'row-reverse',
+        }}
+        icon="arrow-down-drop-circle-outline"
+        labelStyle={{
+          fontSize: 24,
+          textAlign: 'center',
+          lineHeight: 24,
+        }}
+        rippleColor="#00000000"
+        textColor="black"
+        onPress={() => {}}>
+        PLAVE
+      </Button>
+      <IconButton icon="filter" onPress={() => {}} />
+    </Appbar.Header>
+  </View>
+);
+
+const App = () => {
   return (
-    <PaperProvider>
-      <App />
-    </PaperProvider>
+    <SafeAreaProvider>
+      <Fragment>
+        <MainAppBar />
+      </Fragment>
+    </SafeAreaProvider>
   );
-}
+};
 
-AppRegistry.registerComponent(appName, () => Main);
+export default App;
