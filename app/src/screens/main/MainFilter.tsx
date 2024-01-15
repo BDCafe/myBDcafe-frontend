2571e360a5ba524c4959f68f4f3d5167728c4b06
Lee Jihye <jihae0584@gmail.com>
2024-01-16 01:18:47 +0900

    [FEAT] 아이콘에 색상 적용 #1

--- app/src/screens/main/MainFilter.tsx
+++ app/src/screens/main/MainFilter.tsx
@@ -1,29 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Button } from '@rneui/themed';
-import IconCircleDown from '../assets/expand_circle_down.svg';
-import IconFilter from '../assets/candlestick_chart.svg';
+import IconCircleDown from '../../assets/expand_circle_down.svg';
+import IconFilter from '../../assets/candlestick_chart.svg';
 
 const MainFilter = () => {
   return (
     <View style={styles.filterContainer}>
       <Button
-        //type="clear"
+        type="clear"
         containerStyle={styles.genreButton}
         onPress={() => console.log('asdf')}
       >
         <Text style={styles.genreButtonText}>PLAVE</Text>
         <IconCircleDown
-          width={15}
-          height={15}
+          width={20}
+          height={20}
+          color={'#000000'}
         />
       </Button>
       <Button
-        //type="clear"
+        type="clear"
         containerStyle={styles.genreButton}
         onPress={() => console.log('asdf')}
       >
-        <IconFilter />
+        <IconFilter color={'#D2D4DA'} />
       </Button>
     </View>
   );
