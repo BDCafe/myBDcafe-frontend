e2045d1f1ec7b92bc24d7c7054793cc998a0191c
Lee Jihye <jihae0584@gmail.com>
2024-01-15 09:06:28 +0900

    [FEAT] 메인화면 필터 UI 그림

---
+++ app/src/screens/main/MainFilter.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from '@rneui/themed';
+import IconCircleDown from '../assets/expand_circle_down.svg';
+import IconFilter from '../assets/candlestick_chart.svg';
+
+const MainFilter = () => {
+  return (
+    <View style={styles.filterContainer}>
+      <Button
+        //type="clear"
+        containerStyle={styles.genreButton}
+        onPress={() => console.log('asdf')}
+      >
+        <Text style={styles.genreButtonText}>PLAVE</Text>
+        <IconCircleDown
+          width={15}
+          height={15}
+        />
+      </Button>
+      <Button
+        //type="clear"
+        containerStyle={styles.genreButton}
+        onPress={() => console.log('asdf')}
+      >
+        <IconFilter />
+      </Button>
+    </View>
+  );
+};
+
+export default MainFilter;
+
+const styles = StyleSheet.create({
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#ffffff',
+  },
+
+  genreButton: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignContent: 'space-between',
+  },
+
+  genreButtonText: {
+    marginRight: 8,
+    fontFamily: 'nanum_square_neo_deb',
+    fontSize: 24,
+    color: '#000000',
+  },
+});
